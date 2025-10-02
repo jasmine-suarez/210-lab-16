2e4edd8c0586986ade12a85d347ce99f52c97a5c
Jasmine <jazzyysuarez@gmail.com>
2025-10-01 22:30:30 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,60 @@
+// COMSC-210 | Lab 16 | Jasmine Suarez
+// IDE used: VS Code
+
+#include <iostream>
+using namespace std;
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+public:
+    int getRed()            { return red; }
+    void setRed(int r)      { red = r; }
+    int getGreen()          { return green; }
+    void setGreen(int g)    { green = g; }
+    int getBlue()           { return blue; }
+    void setBlue(int b)     { blue = b; }
+
+    void print() {
+        cout << "Red value: " << red << endl;
+        cout << "Green value: " << green << endl;
+        cout << "Blue value: " << blue << endl;
+    }
+};
+
+void print(Color);
+
+int main() {
+    // color 1
+    Color c1;
+    c1.setRed(249);
+    c1.setGreen(52);
+    c1.setBlue(82);
+    c1.print();
+    print(c1);
+
+    // color 2
+    Color c2;
+    c2.setRed(178);
+    c2.setGreen(248);
+    c2.setBlue(41);
+    c2.print();
+    print(c2);
+
+    // color 3
+    Color c3;
+    c3.setRed(3);
+    c3.setGreen(56);
+    c3.setBlue(240);
+    c3.print();
+    print(c3);
+
+    return 0;
+}
+
+void print(Color c) {
+    cout << "(" << c.getRed() << ", " << c.getGreen() << ", "
+         << c.getBlue() << ")" << endl << endl;
+}
